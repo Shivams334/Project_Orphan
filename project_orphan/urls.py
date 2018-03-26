49aa89dc27bfd748c18b9c85775a460a474544bd
Pradhvan <Pradhvanbisht@gmail.com>
2018-03-26 22:48:17 +0530

    Error(Base.html):
    Messed up authentication in base.html

--- project_orphan/urls.py
+++ project_orphan/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 
 from accounts import views as accounts_views
@@ -22,6 +23,7 @@ from orphan import views
 
 urlpatterns = [
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
